723df54e619d6a68841e21f1005298c4380fa5a2
liuyang <944491509@qq.com>
2020-08-10 17:32:08 +0800

    故障来源

--- app/StandAdmin/routes.php
+++ app/StandAdmin/routes.php
@@ -14,10 +14,22 @@ Route::group([
 
     $router->get('/', 'HomeController@index');
 
+    // 区站管理
     $router->group(['prefix' => 'district'], function (Router $router) {
         $router->resource('departments', District\DepartmentController::class);
         $router->resource('task-groups', District\TaskGroupController::class);
         $router->resource('automobiles', District\AutomobileController::class);
         $router->resource('instruments', District\InstrumentController::class);
     });
+
+    // 网络故障
+    $router->group(['prefix' => 'fault'], function (Router $router) {
+        // 故障来源
+        $router->resource('source', Fault\SourceController::class);
+        // 故障性质
+        $router->resource('nature', Fault\NatureController::class);
+        // 故障列表
+        $router->resource('list', Fault\NetworkFaultController::class);
+    });
+
 });
---
+++ database/migrations/2020_08_10_170707_add_menus_in_stand_admin_fault_table.php
@@ -0,0 +1,69 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddMenusInStandAdminFaultTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // 添加一级菜单
+        $menu = [
+            'parent_id' => 0,
+            'order' => 4,
+            'title' => '故障管理',
+            'icon' => 'fa-exclamation-triangle',
+            'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
+            'updated_at' => \Carbon\Carbon::now()->toDateTimeString(),
+        ];
+        $id = DB::table('stand_admin_menu')->insertGetId($menu);
+        $data = [
+            [
+                'parent_id' => $id,
+                'order' => 1,
+                'title' => '故障来源',
+                'icon' => 'fa-soundcloud',
+                'uri' => 'fault/source',
+                'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
+                'updated_at' => \Carbon\Carbon::now()->toDateTimeString(),
+            ],
+            [
+                'parent_id' => $id,
+                'order' => 2,
+                'title' => '故障性质',
+                'icon' => 'fa-code-fork',
+                'uri' => 'fault/nature',
+                'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
+                'updated_at' => \Carbon\Carbon::now()->toDateTimeString(),
+            ],
+            [
+                'parent_id' => $id,
+                'order' => 2,
+                'title' => '故障列表',
+                'icon' => 'fa-list',
+                'uri' => 'fault/list',
+                'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
+                'updated_at' => \Carbon\Carbon::now()->toDateTimeString(),
+            ],
+        ];
+        DB::table('stand_admin_menu')->insert($data);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $title = ['故障管理', '故障来源', '故障性质', '故障列表'];
+        DB::table('stand_admin_menu')->whereIn('title', $title)->delete();
+    }
+}
